1d3c0748f42e92e647b53dd33a37bd306760f158
flaviohfsilva <flavinhovinho2004@gmail.com>
2023-05-17 23:08:41 -0300

    Adicionando as cores que serão utilizadas

---
+++ lib/constants/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+
+//Cores menu e body
+const Color menu = Color(0xFFF6F6F6);
+const Color bodyDark = Color(0xFF363636);
+
+//Cores das fontes e Icons
+const Color textMain = Color(0xFF888888);
+const Color titles = Color(0xFF525252);
+const Color textNavegation = Color(0xFFD9D9D9);
+const Color icons = Color(0xFF676464);
+
+//Cores de detalhes
+const Color orangeDetails = Color(0xFFF29F05);
+const Color orangeNavegation = Color(0xFFF2b705);
+const Color yellowDetails = Color(0xffF2CB05);
+const Color blueDetails = Color(0xFF85F2F2);
+
+//Cores das Linhas
+const Color blueLinhaSul = Color(0xFF1852D6);
+const Color redLinhaCentro = Color(0xFFDA1D27);
+const Color orangeLinhaCentro = Color(0xFFF87647);
+const Color greenLinhaVlt = Color(0xFF259F5E);
+const Color integracaoDiesel = Color(0xFF593E25);
+const Color integracaoDieselDark = Color(0xFF986E47);
