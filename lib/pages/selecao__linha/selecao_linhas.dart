8af7bd41b8c2872dab70d74001df3a7cef913dc9
Gabriela Diniz <gadisa10@outlook.com>
2023-05-26 14:42:07 -0300

    Corrigindo erro no arquivo selecao_linhas

---
+++ lib/pages/selecao__linha/selecao_linhas.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:metroguia/pages/Infomacoes/components/body_info.dart';
+
+
+class selecaoLinha extends StatelessWidget {
+  const selecaoLinha({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BodyInfo(),
+    );
+  }
+}
