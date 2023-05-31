59dfce0a2cd153bcc14c12f5755ac41644acf6f4
flaviohfsilva <flavinhovinho2004@gmail.com>
2023-05-30 23:40:29 -0300

    Atualização das rotas e title home

--- lib/pages/home/components/fav_routes.dart
+++ lib/pages/home/components/fav_routes.dart
@@ -32,7 +32,7 @@ class FavoritesRotas extends StatelessWidget {
                         Row(
                           children: [
                             Padding(
-                              padding: const EdgeInsets.all(8.0),
+                              padding: const EdgeInsets.all(10.0),
                               child: Icon(
                                 Icons.star,
                                 color: Colors.yellow,
@@ -56,33 +56,19 @@ class FavoritesRotas extends StatelessWidget {
                               Icons.directions_subway_outlined,
                               color: icons,
                             ),
-                            Text(
-                                "Estação Afogados",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w700
-                                    )
+                            Padding(
+                              padding: const EdgeInsets.only(right: 20),
+                              child: Text(
+                                  "Estação Afogados",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w700
+                                      )
+                              ),
                             )
                           ],
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Icon(
-                              Icons.directions_bus_outlined,
-                              color: icons,
-                            ),
-                            Text(
-                                "TI PE-15/Afogados",
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w700
-                                )
-                            )
-                          ],
-                        )
                       ],
                     ),
                   ),
@@ -94,17 +80,7 @@ class FavoritesRotas extends StatelessWidget {
                     height: 95,
                     width: 200,
                     child: Container(
-                      decoration:
-                      BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black.withOpacity(0.2),
-                                spreadRadius: 2,
-                                blurRadius: 5.0,
-                                offset: Offset(2.0, 2.0)
-                            )
-                          ]),
+                      color: Colors.white,
                       child: Column(
                         children: [
                           Row(
@@ -112,61 +88,34 @@ class FavoritesRotas extends StatelessWidget {
                               Padding(
                                 padding: const EdgeInsets.all(8.0),
                                 child: Icon(
-                                  Icons.star,
+                                  Icons.star_border_outlined,
                                   color: Colors.yellow,
-                                  size: 25,
+                                  size: 30,
                                 ),
                               ),
-                              Text(
-                                "Linha Sul",
-                                style: TextStyle(
-                                    color: textMain,
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w700
-                                ),
-                              )
                             ],
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
-                              Icon(
-                                Icons.directions_subway_outlined,
-                                color: icons,
-                              ),
-                              Text(
-                                  "Estação Largo da Paz",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w700
-                                  )
+                              Padding(
+                                padding: const EdgeInsets.only(left: 2),
+                                child: Text(
+                                    "Adicione a sua linha e estação \nque você mais usa aqui",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w500
+                                    )
+                                ),
                               )
                             ],
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Icon(
-                                Icons.directions_bus_outlined,
-                                color: icons,
-                              ),
-                              Text(
-                                  "TI Aeroporto/Afogados",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w700
-                                  )
-                              )
-                            ],
-                          )
                         ],
                       ),
                     ),
                   ),
                 ),
-
 
                 SizedBox(width: 20,)
               ],
